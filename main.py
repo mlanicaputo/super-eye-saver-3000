1a32f48028dfbed7957b307b248d53b497a053f1
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-08-27 14:55:37 -0400

    Add window function

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from time import time
+import time
+from tkinter import Button, Label, Tk, ttk
 
 
-
+def window():
+    # configure window
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="LOOK AT SOMETHING 20 FEET AWAY FOR 20 SECONDS!!!").grid(column=0, row=0)
+    ttk.Button(frm, text="Ok, did it!", command=root.destroy).grid(column=1, row=0)
+    ttk.Button(frm, text="Quit", command=exit).grid(column=1, row=1)
+    root.mainloop()
+    
 
 def run():
-    # every 20 minutes, create a window that says "SAVE YOUR EYES"
-    elapsed_time = 0
+    # sleep for 20 minutes
+    # time.sleep(1200)
+    time.sleep(1200)
 
-
-
-
-
-
-
-
+    # create window
+    window()
 
 
 def main():
