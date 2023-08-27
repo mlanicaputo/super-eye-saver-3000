21346b16163de09ad6547ed65c47c6b4931ca761
Milo Lani-Caputo <mlanicaputo@gmail.com>
2023-08-27 14:30:55 -0400

    Add main script

---
+++ main.py
@@ -0,0 +1,24 @@
+from time import time
+
+
+
+
+def run():
+    # every 20 minutes, create a window that says "SAVE YOUR EYES"
+    elapsed_time = 0
+
+
+
+
+
+
+
+
+
+
+def main():
+    run()
+
+
+if __name__ == "__main__":
+    main()
